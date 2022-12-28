b587cb49418fbcb60b52da16f4cb9039558fef29
Nadia <nadiagueu@gmail.com>
2022-12-28 02:11:57 +0000

    week2 day1 exerciceXP6

---
+++ week2/day1/ExerciceXP/exercice6/exercice6.js
@@ -0,0 +1,55 @@
+5 + "34"
+// Prediction: 534
+// Actual: 534
+
+5 - "4"
+// Prediction: 1
+// Actual: 1
+
+10 % 5
+// Prediction:0
+// Actual:0
+
+5 % 10
+// Prediction: 0
+// Actual: 0
+
+"Java" + "Script"
+// Prediction: JavaScript
+// Actual: JavaScript
+
+" " + " "
+// Prediction: ""
+// Actual: ""
+
+" " + 0
+// Prediction: 0
+// Actual: 0
+
+true + true
+// Prediction: 2
+// Actual: 2
+
+true + false 
+// Prediction: 1
+// Actual: 1
+
+false + true
+// Prediction: 1
+// Actual: 1
+
+false - true
+// Prediction: -1
+// Actual: -1
+
+!true
+// Prediction: false
+// Actual: false
+
+3 - 4
+// Prediction: -1
+// Actual: -1
+
+"Bob" - "bill" 
+// Prediction: NaN
+// Actual: NaN
